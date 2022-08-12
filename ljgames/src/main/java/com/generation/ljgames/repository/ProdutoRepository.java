2014fc115e7d4f8f7618f981ef4bae8390164a9f
unknown <ferreira.ellenk@gmail.com>
2022-08-12 14:51:33 -0300

    Loja de Games - Spring - inclusão da camada Security e Service

---
+++ ljgames/src/main/java/com/generation/ljgames/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package com.generation.ljgames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.ljgames.model.ProdutoModel;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+	
+		public List<ProdutoModel>findAllByNomeContainingIgnoreCase (String produto);
+	}
+
+
+
